0581b065c4b8d91775d5c151cd2632ff78f083f3
hsq <hsqyc@163.com>
2018-06-24 22:45:15 +0800

    try to understand keras.model

--- load.py
+++ load.py
@@ -2,60 +2,28 @@ import tensorflow as tf
 from keras.layers import *
 from keras import backend as K
 from keras.models import Model, load_model
+from keras.optimizers import Adam
 
-from PIL import Image
-import cv2
-# image = Image.open('dog.jpg')
-# image = image.resize((416, 416), Image.BICUBIC)
-# image_data = np.array(image, dtype='float32')
-# image_data = image_data / 255.
-# image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
-img = cv2.imread('dog.jpg')
-img = img[:, :, ::-1]  # RGB image
-im_sized = cv2.resize(img, (416, 416))
-image_data = np.array(im_sized, dtype='float32')
-image_data /= 255.
-image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+from train import yolo3_loss
+from data import data_generator
+# def yolo3_loss(args):
+#     return args
 
-model = load_model('model.h5', compile=False)
 
-num_classes = 80
+C = 80
 
-anchors = tf.constant([10, 13, 16, 30, 33, 23, 30, 61, 62, 45, 59, 119, 116, 90, 156, 198, 373, 326],
-                      dtype='float', shape=[1, 1, 1, 9, 2])
-input_shape = K.shape(model.output[2])[1:3] * 32
+y_true = list()
+y_true.append(Input(name='input0', shape=[13, 13, 3, (5 + C)]))
+y_true.append(Input(name='input1', shape=[26, 26, 3, (5 + C)]))
+y_true.append(Input(name='input2', shape=[52, 52, 3, (5 + C)]))
 
-boxes = list()
-scores = list()
-classes = list()
-for i in range(3):  # 52 26 13
-    anchor = anchors[..., 3 * i:3 * (i + 1), :]
-    feats = model.output[i]
+model_load = load_model('model.h5', compile=False)
+loss_layer = Lambda(yolo3_loss, name='loss_layer')([*model_load.output, *y_true])
+model = Model(inputs=[model_load.input, *y_true], outputs=loss_layer)
 
-    grid_shape = K.shape(feats)[1:3]  # height, width
-    grid_y = K.tile(K.reshape(K.arange(0, stop=grid_shape[0]), [-1, 1, 1, 1]),
-                    [1, grid_shape[1], 1, 1])
-    grid_x = K.tile(K.reshape(K.arange(0, stop=grid_shape[1]), [1, -1, 1, 1]),
-                    [grid_shape[0], 1, 1, 1])
-    grid = K.concatenate([grid_x, grid_y])
-    grid = K.cast(grid, K.dtype(feats))
-
-    feats = K.reshape(
-        feats, [-1, grid_shape[0], grid_shape[1], 3, num_classes + 5])
-
-    # Adjust preditions to each spatial grid point and anchor size.
-    box_xy = (K.sigmoid(feats[..., :2]) + grid) / K.cast(grid_shape[::-1], K.dtype(feats))
-    box_wh = K.exp(feats[..., 2:4]) * anchor / K.cast(input_shape[::-1], K.dtype(feats))
-    box_confidence = K.sigmoid(feats[..., 4:5])
-    box_class_probs = K.sigmoid(feats[..., 5:])
-
-    box_scores = box_confidence * box_class_probs
-    max_scores = tf.reduce_max(box_scores, axis=-1)
-
-    scores.append(max_scores)
-
-sess = K.get_session()
-s = sess.run(scores, feed_dict={model.input: image_data,
-                                K.learning_phase(): 0})
+model.compile(optimizer=Adam(lr=1e-3), loss=lambda y, y_: y_)
+model.fit_generator(generator=data_generator(images_path="D:/DeepLearning/data/VOCdevkit/VOC2012/JPEGImages/",
+                   annotations_path="D:/DeepLearning/data/VOCdevkit/VOC2012/Annotations/", batch_size=16),
+                    )
 
 exit()
